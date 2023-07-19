912ea48cd723c1dee1ba07999898a1f715375668
Mahdi Mazaheri <mahdi.mazaheri8383@gmail.com>
2023-07-19 14:08:22 +0330

    [ADD] circle avatar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,9 @@ import { AppComponent } from './app.component';
 import { FlowSnackbarComponent } from './components/flow-snackbar/flow-snackbar.component';
 import {SearchModule} from "./components/search/search.module";
 import { ExpandOverlayComponent } from './components/expand-overlay/expand-overlay.component';
-import { MaxflowDialogComponent } from './components/maxflow-dialog/maxflow-dialog.component';
+import { CircleAvatarComponent } from './components/circle-avatar/circle-avatar.component';
 @NgModule({
-  declarations: [AppComponent, FlowSnackbarComponent, ExpandOverlayComponent, MaxflowDialogComponent],
+  declarations: [AppComponent, FlowSnackbarComponent, ExpandOverlayComponent, CircleAvatarComponent],
     imports: [BrowserModule, AppRoutingModule, SearchModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/circle-avatar/circle-avatar.component.ts
@@ -0,0 +1,12 @@
+import {Component, HostBinding, Input} from '@angular/core';
+import {style} from "@angular/animations";
+
+@Component({
+    selector: 'app-circle-avatar',
+    templateUrl: './circle-avatar.component.html',
+    styleUrls: ['./circle-avatar.component.scss']
+})
+export class CircleAvatarComponent {
+    @Input() src: string = "assets/icons/account.svg";
+    @Input() size: number = 10;
+}
